f7f0407d16516356d033ac651ca0d491374edf2b
Boons <joseph.shering@gmail.com>
2024-05-24 16:41:32 -0700

    Added Actions and Contexts for Controller

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,6 +2,7 @@
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -27,5 +28,30 @@ void AAuraPlayerController::BeginPlay()
 	InputModeData.SetHideCursorDuringCapture(false);
 
 	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EIC = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EIC->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D Axis = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+
+	const FRotator Yaw = FRotator(0.f, Rotation.Yaw, 0.f);
+	const FVector ForwardDirection = FRotationMatrix(Yaw).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(Yaw).GetUnitAxis(EAxis::Y);
+
+	if(APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, Axis.Y);
+		ControlledPawn->AddMovementInput(RightDirection, Axis.X);
+	}
 }
 
